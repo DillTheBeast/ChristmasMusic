7ce85ba69900b26763015fdddcb8494b4f945260
DillTheBeast <dillonmaltese@gmail.com>
2022-12-08 09:26:17 -0500

    t

--- src/christmasmusic/Controller.java
+++ src/christmasmusic/Controller.java
@@ -1,16 +1,44 @@
 package christmasmusic;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
 
+    //All of the song buttons + shuffle button
     @FXML
-    private Label label;
+    private Button Button1;
+    @FXML
+    private Button Button2;
+    @FXML
+    private Button Button3;
+    @FXML
+    private Button Button4;
+    @FXML
+    private Button Button5;
+    @FXML
+    private Button Button6;
+    @FXML
+    private Button Button7;
+    @FXML
+    private Button Button8;
+    @FXML
+    private Button Button9;
+    @FXML
+    private Button Button10;
+    @FXML
+    private Button Button11;
+    @FXML
+    private Button Button12;
+    @FXML
+    private Button Button13;
+    @FXML
+    private Button Button14;
+    @FXML
+    private Button shuffleButton;
 
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
     }
 }
